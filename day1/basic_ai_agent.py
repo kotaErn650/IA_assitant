bbbf740a497c84179c32d679998d7eef6327ab0a
kotaErn650 <kota012011@gmail.com>
2025-09-06 21:38:52 -0500

    agreandop memoria a la IA

--- day1/basic_ai_agent.py
+++ day1/basic_ai_agent.py
@@ -1,4 +1,8 @@
 from langchain_ollama import OllamaLLM  
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.prompts import PromptTemplate
+
+
 
 # load Ai Model from Ollama
 
@@ -6,13 +10,25 @@ from langchain_ollama import OllamaLLM
 #inicializa el modelo que se esta usando
 llm = OllamaLLM(model ="mistral")
 
+#inicializar memoria
+chat_history = ChatMessageHistory() # esto conserva las conversaciones con la IA
+prompt = PromptTemplate(
+    input_variables=["chat_history","question"],
+    template="Conversacion Previa: {chat_history}\n\n User: {question}\n\nAu :}"
+)
 
-print("/n Bienvenido a tu agente de is")
+#funcuion para  memoria de IA
+def run_chain(question):
+    chat_history_text= "\n".join([f"{msg.type.capitaliza()}: {msg.content}" for msg in chat_history.messages])
 
-while True: 
-    question = input("realiza tu pregunta o escrbe 'exit para salir: ")
-    if question.lower()=='exit':
-        print("saliendo de.....")
-        break
-    response = llm.invoke(question)
-    print("/n Ai Respuesta : ", response)
+
+
+# print("/n Bienvenido a tu agente de is")
+
+# while True: 
+#     question = input("realiza tu pregunta o escrbe 'exit para salir: ")
+#     if question.lower()=='exit':
+#         print("saliendo de.....")
+#         break
+#     response = llm.invoke(question)
+#     print("/n Ai Respuesta : ", response)
